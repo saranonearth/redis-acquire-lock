c434dedab07c86d6dafa1cf16a316fe5b6d84b10
0xsaranonearth <saran.s@pillow.fund>
2023-01-03 02:26:26 +0530

    Setup | get key utils

---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+import { KeyType } from "./types";
+
+export function getKey(keyValue: KeyType, args: any[]): string {
+  let key = "";
+
+  if (typeof keyValue === "object" && Array.isArray(keyValue)) {
+    args.forEach((value, index) => {
+      if (keyValue.indexOf(index) !== -1) {
+        key += value + "_";
+      }
+    });
+    return key;
+  }
+
+  if (typeof keyValue === "string") {
+    key = keyValue;
+    return key;
+  }
+
+  if (typeof keyValue === "function") {
+    return keyValue(args);
+  }
+  throw new Error("@AcquireLock - invalid key type");
+}
+
+function isPromise(object: any): object is Promise<any> {
+    return object && Promise.resolve(object) === object;
+  }
